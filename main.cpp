b1cb5aff79869cb908b361fedf91800cb1700ab8
Cross, Winter Isaac <wi928rsnowflake@gmail.com>
2024-07-01 10:27:01 -0400

    cleaned and tested code

--- main.cpp
+++ main.cpp
@@ -1,16 +1,11 @@
 //main.cpp
 #include <iostream>
-// #include <stdio.h>
-// #include <string>
-// #include <numeric>
 
 //function prototypes
 void do_first_black_jack_analysis();
-
 float prob_first_card_is_jack(int,int);
 float prob_card_is_first_jack(int,int,int);
 float prob_team_wins(int,int,int,int);
-
 int get_user_cards();
 int get_user_jacks(int);
 int get_user_teams();
@@ -44,38 +39,39 @@ void do_first_black_jack_analysis(){
 	int teams = get_user_teams();
 	if (teams==-1){return;}
 	//analyze the game and print the results
-	std::cout << "\nIn the game with "<<cards<<" cards, "<<jacks<<" jacks, and "<<teams<<" teams, the probability that each team wins is:\n";
+	std::cout << "\nIn the game with "<<cards<<" cards, "<<jacks<<" black jacks, and "<<teams<<" teams, the probability that each team wins is:\n";
 	for (int teamNumber=1;teamNumber<teams+1;teamNumber++){
 		float winProb = prob_team_wins(cards,jacks,teams,teamNumber);
 		std::cout << "Team "<<teamNumber<<" wins "<<100*winProb<<"% of the time.\n";
 	}
 	std::cout<<"\n";
 }
-
-//!!!For efficiency, should save results in a global table
+//calculation funcs
 float prob_first_card_is_jack(int cards,int jacks){
 	return ((float)jacks/cards);
 }
 float prob_card_is_first_jack(int cards,int jacks,int cardNumber){//cardNumber>=1
+	//if there is more than one black jack, the final card cannot be the first black jack.
 	if (jacks>cards-(cardNumber-1)){
 		return 0;
 	}
+	//Calculate coefficient based on odds all previous cards are not first black jack
 	float coefficient = 1.0;
 	if (cardNumber>1){
-		for (int index=0;index<cardNumber-1;index++){
+		for (int index=0;index<cardNumber-1;index++){//!!Can be quicker if we save the previous for-loop value and use it to calculate the next
 			coefficient*=1-prob_first_card_is_jack(cards-index,jacks);
 		}
 	}
-	// std::cout<<"prob for cardNumber "<<cardNumber<<" is: "<<coefficient*prob_first_card_is_jack(cards-(cardNumber-1),jacks)<<"\n";
 	return coefficient*prob_first_card_is_jack(cards-(cardNumber-1),jacks);
 }
 float prob_team_wins(int cards,int jacks,int teams,int teamNumber){//teamNumber>=1
 	//find number of cards this team gets in this game
-	int cardsForTeam = cards/teams;//!!!Verify this is right
+	int cardsForTeam = cards/teams;
 	int remainder = cards%teams;
 	if (remainder>=teamNumber){
 		cardsForTeam++;
 	}
+	//no cards means winning is impossible
 	if (cardsForTeam==0){
 		return 0.0;
 	}
@@ -106,22 +102,23 @@ int get_user_cards(){
 	return cards;
 }
 int get_user_jacks(int cards){
-	//ask user for number of jacks
+	//'jacks' and 'black jacks' here means winning cards. AKA, the cards that have the team who turn them up win the game
+	//ask user for number of black jacks
 	int jacks;
-	std::cout << "Enter the number of jacks (winning cards) in the deck: ";
+	std::cout << "Enter the number of black jacks in the deck: ";
 	std::cin >> jacks;
 	if (std::cin.fail()){
-		std::cout<<"Error: Your deck must have an integer number of winning cards.\n\n";
+		std::cout<<"Error: Your deck must have an integer number of black jacks.\n\n";
 		std::cin.clear();
 		std::cin.ignore(100,'\n');
 		return -1;
 	}
 	if (jacks<1){
-		std::cout << "Error: Your deck must have at least 1 jack.\n\n";
+		std::cout << "Error: Your deck must have at least 1 black jack.\n\n";
 		return -1;
 	}
 	if (jacks>cards){
-		std::cout << "Error: You cannot have more jacks than cards.\n\n";
+		std::cout << "Error: You cannot have more black jacks than total cards in the deck.\n\n";
 		return -1;
 	}
 	return jacks;
